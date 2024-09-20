bf9dfb873840eb7416c3820d2f105abc89e90a50
Your Name <you@example.com>
2024-09-19 19:26:14 -0500

    (nueva):configuracion del proyecto spring

---
+++ src/main/java/co/edu/ucentral/myrecycled/MyrecicledApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.myrecycled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecicledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrecicledApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ucentral/myrecycled/MyrecicledApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.myrecycled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrecicledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
